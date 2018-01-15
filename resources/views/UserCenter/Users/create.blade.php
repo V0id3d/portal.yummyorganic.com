ddeba07566aa10736b09b62301375c8721dc8ba6
Carlos Quinones <CQuinones@Qzion.net>
2018-01-15 17:12:30 -0500

    --Updated Upto Customer Center--

--- resources/views/UserCenter/Users/create.blade.php
+++ resources/views/UserCenter/Users/create.blade.php
@@ -65,6 +65,7 @@
                             </div>
                         </div>
 
+
                         <div class="row">
                             <div class="col-6">
                                 <div class="form-group">
