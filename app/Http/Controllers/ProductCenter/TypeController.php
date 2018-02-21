aed05d7978051ffd0efc0c4a10b9804ce09f680c
Carlos Quinones <CQuinones@Qzion.net>
2018-02-21 11:01:14 -0500

    Working on Product Centers [Brands]

--- app/Http/Controllers/ProductCenter/TypeController.php
+++ app/Http/Controllers/ProductCenter/TypeController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\ProductCenter;
 use App\ProductCenter\Type;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
 
 class TypeController extends Controller
 {
+    protected function validator(array $data, $record = null)
+    {
+        return Validator::make($data, [
+            'name' => (is_null($record)) ? 'required|string|max:255|unique:types' : 'required|string|max:255|unique:types,name,' . $record->id,
+        ]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -36,6 +43,8 @@ class TypeController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validator($request->all())->validate();
+
         $newType = Type::create([
             'name' => $request->name,
             'description' => $request->description
@@ -58,24 +67,33 @@ class TypeController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param Type $selected_type
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function edit($id)
+    public function edit(Type $selected_type)
     {
-        //
+        return view('ProductCenter.Type.edit', compact('selected_type'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param Type $selected_type
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Type $selected_type)
     {
-        //
+        $this->validator($request->all(), $selected_type)->validate();
+
+        $updatedType = $selected_type->update([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+
+        return redirect(route('ProductCenter.Index'));
     }
 
     /**
---
+++ resources/views/ProductCenter/Type/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.admin')
+
+@section('main-content')
+    <header class="header no-border" xmlns="http://www.w3.org/1999/html">
+        <div class="header-info">
+            <div class="left">
+                <h2 class="header-title"><strong>Product Center</strong> Edit Type</h2>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="main-content">
+        <div class="row">
+
+            <div class="col-12">
+                <div class="card card-shadowed form-type-material">
+                    <header class="card-header">
+                        <h4 class="card-title"><strong>Create</strong> Type</h4>
+                    </header>
+
+                    <div class="card-body">
+                        <form action="{{ route('ProductCenter.Type.Update', $selected_type) }}" method="POST" id="type">
+                            {{ csrf_field() }}
+                            {{ method_field('patch') }}
+
+
+
+                            <div class="row">
+
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" name="name" value="{{ old('name', $selected_type->name) }}">
+                                        <label>Name</label>
+                                        @if ($errors->has('name'))
+                                            <div class="invalid-feedback">{{ $errors->first('name') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" rows="6" name="description">{{ old('description', $selected_type->description) }}</textarea>
+                                        <label>Description</label>
+                                        @if ($errors->has('description'))
+                                            <div class="invalid-feedback">{{ $errors->first('description') }}</div>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+
+                        </form>
+
+
+                    </div>
+                    <footer class="card-footer text-right p-0">
+                        <div class="btn-group">
+                            <button class="btn btn-square btn-primary no-radius" type="submit" form="type"><i class="fa fa-save"></i></button>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+@endsection
